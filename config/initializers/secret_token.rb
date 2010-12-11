38a2666119d47b86df6375c91aa96206e9aec088
Baraa Basata <bsb@ais.org>
2010-12-10 23:02:35 -0500

    rails new baraabasata

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baraabasata::Application.config.secret_token = '87bcfa51c90016b581bf9d23d5fbcd6784261ba9283a010b8178ea35e06107b724b949d7d4601c2971b4a2dbd7e013237e13899589e7423b937515788ed83889'
